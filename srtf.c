87da9c0b11da6e8cbb215f1f4bb32b9dee60a392
Chandra Christina <49263398+chris1408@users.noreply.github.com>
2020-04-02 02:19:45 +0530

    srtf
    implemented the srtf

--- srtf.c
+++ srtf.c
@@ -3,17 +3,49 @@
 
 int main()
 {
-  int at[10],bt[10],rt[10],endTime,i,smallest;
-  int remain=0,n,time,sum_wait=0,sum_turnaround=0;
-  printf("Enter no of Processes : ");
+  int at[10],bt[10],rt[10],end,i,s;
+  int r=0,n,time,sumw=0,sumt=0;
+  printf("No of Processes : ");
   scanf("%d",&n);
   
   for(i=0;i<n;i++)
   {
-    printf("Enter arrival time for Process P%d : ",i+1);
+    printf("Arrival time for Process P%d : ",i+1);
     scanf("%d",&at[i]);
     printf("Enter burst time for Process P%d : ",i+1);
     scanf("%d",&bt[i]);
     rt[i]=bt[i];
   }
+
+  printf("\n\nProcess\t|Turnaround Time| Waiting Time\n\n");
+  rt[9]=9999;
+  for(time=0;r!=n;time++)
+  {
+      s=9;
+      
+      for(i=0;i<n;i++)
+      {
+        if(at[i]<=time && rt[i]<rt[s] && rt[i]>0)
+        {
+          s=i;
+        }
+      }
+      
+      rt[s]--;
+      
+      if(rt[s]==0)
+        {
+          r++;
+          end=time+1;
+          printf("\nP[%d]\t|\t%d\t|\t%d",s+1,end-at[s],end-bt[s]-at[s]);
+          sumw+=end-bt[s]-at[s];
+          sumt+=end-at[s];
+        }
+   }
+  
+
+  printf("\n\nAverage waiting time = %f\n",sumw*1.0/n);
+  printf("Average Turnaround time = %f",sumt*1.0/5);
+  return 0;
+
 }
